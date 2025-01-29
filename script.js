0948c4f0b3b73eb98ec2f35129dd2e6c958fa3ea
Samuel German <101644468+SamuelGerman@users.noreply.github.com>
2025-01-28 21:01:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Adicione interações JavaScript aqui, se necessário
+// Exemplo: Scroll suave para seções
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
